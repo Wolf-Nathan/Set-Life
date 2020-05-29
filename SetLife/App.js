bc180d8e7d705d711a24f6c5fcbcf08c09ab9c6e
nelu <wf.nelu@yandex.ru>
2020-05-29 16:10:07 +0200

    add Provider + install 'redux'

--- SetLife/App.js
+++ SetLife/App.js
@@ -11,7 +11,7 @@ export default function App() {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
-    </Provider>*/
+    </Provider> */
   );
 }
 
