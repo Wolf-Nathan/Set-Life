61617c955d40af6234aa31eeb6407122c2d02689
balzacbdmt <balzac.baudemont@gmail.com>
2020-06-08 03:01:17 +0200

    Creating RowTask component and structuring Home page

--- SetLife/Components/Home.js
+++ SetLife/Components/Home.js
@@ -1,16 +1,84 @@
 // Components/Home.js
 
 import React from 'react'
-import {Text, View} from "react-native";
+import {Text, View, StyleSheet, FlatList} from "react-native";
+import Constants from 'expo-constants';
+import RowTask from './RowTask';
 
 class Home extends React.Component {
     render() {
+        const date = new Date().toDateString();
+        const listDayTasks = ["task1", "task2"];
+        //TODO get from reducers day tasks.
+
         return (
-            <View>
-                <Text>Bel accueil :></Text>
+            <View style={styles.container}>
+                <Text style={styles.helloText}>Hello Gringo</Text>
+                <Text style={styles.dateText}>{date}</Text>
+                {
+                    listDayTasks.length ?
+                        <View>
+                            <Text style={styles.dayProgramText}>Your day program</Text>
+                            <FlatList
+                                data={listDayTasks}
+                                renderItem={({ item }) => {
+                                    return(
+                                        <RowTask item={item} />
+                                    )}
+                                }
+                                keyExtractor={(item, index)=> index}
+                                />
+                        </View>
+                    :
+                        <View style={styles.dayOffContainer}>
+                            <Text style={styles.freeDayText}>You are free today :D</Text>
+                            <Text style={styles.freeDayLabel}>See you tomorrow !</Text>
+                        </View>
+                }
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#F2F2F2',
+        flex: 1,
+        paddingTop: Constants.statusBarHeight
+    },
+    helloText: {
+        fontFamily: 'Montserrat',
+        color: '#344644',
+        fontSize: 30,
+        textAlign: 'center'
+    },
+    dateText: {
+        fontFamily: 'Montserrat',
+        color: '#77897F',
+        fontSize: 24,
+        textAlign: 'center'
+    },
+    dayProgramText: {
+        fontFamily: 'Montserrat',
+        color: '#77897F',
+        fontSize: 22,
+        marginLeft: 10
+    },
+    dayOffContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    freeDayText: {
+        fontFamily: 'Montserrat',
+        color: '#1B5044',
+        fontSize: 26,
+    },
+    freeDayLabel: {
+        fontFamily: 'Montserrat',
+        color: '#344644',
+        fontSize: 24,
+    }
+});
 
 export default Home;
---
+++ SetLife/Components/RowTask.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import { StyleSheet, View, Text, TouchableWithoutFeedback } from 'react-native';
+import { Dimensions } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const { width } = Dimensions.get('window');
+
+export default class rowTask extends Component {
+
+    render() {
+        //TODO get infos of task from props and display it.
+
+        return (
+            <View style={styles.row}>
+                <View style={styles.opt_btn}>
+                    <TouchableWithoutFeedback>
+                        <MaterialCommunityIcons name="dots-vertical" color={'#F2F2F2'} size={30} />
+                    </TouchableWithoutFeedback>
+                </View>  
+                <Text style={styles.title}>Meal</Text>
+                <Text style={styles.timeText}>07h45 - 08h15</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    row: {
+        backgroundColor: '#1B5044',
+        borderRadius: 15,
+        padding: 5,
+        paddingLeft: 15,
+        width: width-20,
+        margin: 5,
+        alignSelf: 'center'
+        
+    },
+    title: {
+        fontFamily: 'Montserrat',
+        fontSize: 18,
+        color: '#F2F2F2'
+    },
+    timeText: {
+        fontFamily: 'Montserrat',
+        fontSize: 13,
+        color: '#F2F2F2'
+    },
+    opt_btn: {
+        zIndex: 1,
+        padding: 5,
+        margin: 5,
+        position: 'absolute',
+        right: -5,
+        top: 3,
+    }
+});
