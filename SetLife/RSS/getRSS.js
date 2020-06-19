a469159f41328d2c3051c4a9de45228e642fdba8
Wolf Nathan <nathan-w@hotmail.fr>
2020-06-19 11:17:13 +0200

    Catch rss error

--- SetLife/RSS/getRSS.js
+++ SetLife/RSS/getRSS.js
@@ -6,11 +6,20 @@ export default async function getRSS() {
   // allowUniversalAccessFromFileURLs= true;
   // await fetch('http://www.nasa.gov/rss/dyn/breaking_news.rss', {mode: "cors", headers: new Headers({'Access-Control-Allow-Origin' : '*'})})
   // .setHeader('Access-Control-Allow-Origin', '*')
-  const response = await fetch('http://feeds.bbci.co.uk/news/world/rss.xml', { method: 'GET', headers: { 'Access-Control-Allow-Origin': '*' } });
-  const result = await response.text();
-  const rss = await rssParser.parse(result);
-  console.log(`title : ${rss.title}`);
-  console.log(`items : ${rss.items.length}`);
+  try {
+    const response = await fetch('http://feeds.bbci.co.uk/news/world/rss.xml', {
+      method: 'GET',
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      // sendHeadersToHosts: ['http://feeds.bbci.co.uk/news/world/rss.xml']
+    });
+    const result = await response.text();
+    const rss = await rssParser.parse(result);
+    console.log(`title : ${rss.title}`);
+    console.log(`items : ${rss.items.length}`);
+  } catch (err) {
+    console.error('RSS not found');
+    console.error(err);
+  }
   /* then((response) => response.text())
        .then((responseData) => rssParser.parse(responseData))
        .then((rss) => {
